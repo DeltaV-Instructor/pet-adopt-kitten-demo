d0bd04fa500b49bde34dc9f92b3a3e94d31ac076
tektechnologies <info@tektechnologies.com>
2022-11-12 13:35:01 -0600

    add constructor function

--- js/app.js
+++ js/app.js
@@ -2,59 +2,37 @@
 console.log('js file loaded.');
 
 
-
 // Problem Domain: the Cedar Rapids Kitten Rescue has tons of kittens who need good homes. One of the best ways to reach prospective adoptive homes is to have profiles for each kitten available on a website. There are hundreds of kittens, though, and only a few volunteers; it's too time-consuming to hand-code each kitten's profile on their website. They need a better way.
 
-//Each Kitten's profile should have:
-// Name
-// Breed
-// - gender   -- isFixed.
-// - random age assigned
-// a list of what they like to play with.
-// an image
-// is good with kids
-// is good with dogs
-// is good with other animals
 
+//create constructor function to create our pet object for us.
+function Pet(petName, breed, imageName,interests, isGoodWithKids, isGoodWithDogs, isGoodWithAnimals){
+  this.petName = petName;
+  this.breed = breed;
+  this.imageName = imageName;
+  this.interests = interests;
+  this.isGoodWithKids = isGoodWithKids;
+  this.isGoodWithDogs = isGoodWithDogs;
+  this.isGoodWithAnimals = isGoodWithAnimals;
 
-let petOne = {
-  petName: 'Tiger',
-  breed: 'Tiger',
-  imageName: 'diabloBlanco',
-  interests: ['Sun Light', 'Mouse Toys', 'Red dots'],
-  isGoodWithKids: true,
-  isGoodWithDogs: false,
-  isGoodWithAnimals: true,
-  // age: ' 7 months'
-  setAge: function(){
-    this.age = randomAge(3,12) + ' Months old.';
-  }
+}
+//each time our new operator is called we are creating 'an instance of our objects'
+let petOne = new Pet('Tom','Tiger', 'diabloBlanco',['Sun Light', 'Mouse Toys', 'Red dots'],true, false,true);
+let petTwo = new Pet('Garfield','LazyCat', 'jumper',['Cat nip', 'strings', 'food'],true, false,true);
+let petThree = new Pet('Sylvester','sadCat', 'tommyBob',['birds', 'wands', 'naps'],true, false,true);
+
+Pet.prototype.setAge = function(){
+  this.age = randomAge(3,12) + ' Months old.';
 };
-petOne.setAge();
-console.log(petOne);
 
 
-
-
-let petTwo = {
-  petName: 'Fluffy',
-  breed: 'Tabby',
-  imageName: 'tommyBob',
-  interests: ['Rainy Days','Running','Yarn'],
-  isGoodWithKids: true,
-  isGoodWithDogs: true,
-  isGoodWithAnimals: true,
-  setAge: function(){
-    this.age = randomAge(3,12) + ' Months old.';
-  }
+Pet.prototype.getInterests = function(){
+  //grab a random array index and return that interest
+  let randomArrayIndex =  Math.floor(Math.random() * this.interests.length);
+  console.log('index num: ',randomArrayIndex);
 };
-petTwo.setAge();
-console.log(petTwo);
 
 
-//adding properties to our objects outside of our object creation.
-petOne.isFixed = false;
-petTwo.isFixed = true;
 
 
 //helper function
@@ -63,48 +41,99 @@ function randomAge(minMonth, maxMonth){
 }
 
 
-//1. Create DOM elements and render then in the html
-//2. create a new element and append it to the parent element <section>
-//3. create the child elements h3, ul, image, to render a specific pet to the document.
+petOne.setAge();
+petOne.getInterests();
+petTwo.setAge();
+petTwo.getInterests();
+petThree.setAge();
+petThree.getInterests();
 
-
-let parentElement = document.getElementById('kittenProfiles');
-console.log(parentElement);
-
-//create an article tag
-let article = document.createElement('article');
-// console.log(article);
-
-
-//section    add to page    article         lets add the new article tag to the section
-parentElement.appendChild(article);
-
-//h3
-let h3 = document.createElement('h3');
-// add the content
-h3.textContent = petOne.petName;
-article.appendChild(h3);
+// console.log(petOne, petTwo, petThree);
 
 
 
-//creat ul
-let petUL = document.createElement('ul');
-article.appendChild(petUL);
-for(let i = 0; i < petOne.interests.length; i++){
-  // creat li
-  let petLi = document.createElement('li');
-  // set the text content
-  petLi.textContent = petOne.interests[i];
-  //append
-  petUL.appendChild(petLi);
 
+Pet.prototype.render = function(){
+
+  let parentElement = document.getElementById('kittenProfiles');
+  let article = document.createElement('article');
+  parentElement.appendChild(article);
+  let h3 = document.createElement('h3');
+  h3.textContent = this.petName;
+  article.appendChild(h3);
+  let petUL = document.createElement('ul');
+  article.appendChild(petUL);
+
+  for(let i = 0; i < this.interests.length; i++){
+    let petLi = document.createElement('li');
+    petLi.textContent = this.interests[i];
+    petUL.appendChild(petLi);
+  }
+
+  let petImage = document.createElement('img');
+  petImage.setAttribute('src', 'images/'+ this.imageName + '.jpeg');
+  petImage.setAttribute('alt', 'Adopt our kittens.');
+  article.appendChild(petImage);
+
+  ///////////
+  //create table for pets
+  let petTable = document.getElementById('adoptPets-table');
+  let petRow = document.createElement('tr');
+  let nameCell = document.createElement('td');
+  nameCell.textContent = this.petName;
+  petRow.appendChild(nameCell);
+
+  let breedCell = document.createElement('td');
+  breedCell.textContent = this.breed;
+  petRow.appendChild(breedCell);
+
+  let ageCell = document.createElement('td');
+  ageCell.textContent = this.age;
+  petRow.appendChild(ageCell);
+
+  let interestsCell = document.createElement('td');
+  interestsCell.textContent = this.interests;
+  petRow.appendChild(interestsCell);
+  petTable.appendChild(petRow);
+
+};
+
+
+let allPets = [petOne, petTwo, petThree];
+console.log('all PETS: ',allPets);
+
+
+for(let i = 0; i < allPets.length; i++){
+  allPets[i].render();
 }
 
 
-//create image
-let petOneImage = document.createElement('img');
-//src and alt
-petOneImage.setAttribute('src', 'images/' + petOne.imageName + '.jpeg');
-petOneImage.setAttribute('alt', 'Adopt our kittens.');
-//append the image
-article.appendChild(petOneImage);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
